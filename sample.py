61c9eced211434df3b0ef20e0849929e31c48ada
AshitoshWadalkonde <wadalkondea@gmail.com>
2024-11-27 15:07:11 +0530

    new file updated

---
+++ sample.py
@@ -0,0 +1,9 @@
+# Get input from the user
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area
+area = length * width
+
+# Display the result
+print(f"The area of the rectangle is: {area}")
